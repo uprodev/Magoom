ca27882f4cf2bc31e792d2b67bf04767598042c4
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-06-27 11:36:13 +0300

    css js updated

--- magoom_upro/js/app.js
+++ magoom_upro/js/app.js
@@ -725,82 +725,6 @@ window.addEventListener("DOMContentLoaded", () => {
 	initCssVariableWidth();
 	initParallax();
 
-
-	// ==== components =====================================================
-	{
-    const spoilers = document.querySelectorAll('[data-spoiler]');
-    if (spoilers.length) {
-        spoilers.forEach(spoiler => {
-            const swiper = spoiler.closest('[data-scroll-container]')?.swiper;
-            let isOneActiveItem = spoiler.dataset.spoiler.trim() === 'one' ? true : false;
-            let triggers = spoiler.querySelectorAll('[data-spoiler-trigger]');
-            if (triggers.length) {
-                triggers.forEach(trigger => {
-                    let parent = trigger.parentElement;
-                    let content = trigger.nextElementSibling;
-
-                    // init
-                    if (trigger.classList.contains('active')) {
-                        content.style.display = 'block';
-                        parent.classList.add('active');
-                    }
-
-                    trigger.addEventListener('click', (e) => {
-                        e.preventDefault();
-                        parent.classList.toggle('active');
-                        trigger.classList.toggle('active');
-                        content && slideToggle(content, 300);
-                        swiper && setTimeout(() => swiper.update(), 300);
-
-                        if (isOneActiveItem) {
-                            triggers.forEach(i => {
-                                if (i === trigger) return;
-
-                                let parent = i.parentElement;
-                                let content = i.nextElementSibling;
-
-                                parent.classList.remove('active');
-                                i.classList.remove('active');
-                                content && slideUp(content, 300);
-                                swiper && setTimeout(() => swiper.update(), 300);
-                            })
-                        }
-                    })
-                })
-            }
-        })
-    }
-
-    const radioSpoilers = document.querySelectorAll('[data-radio-spoiler]');
-    radioSpoilers.forEach(radioSpoiler => {
-        let triggers = Array.from(radioSpoiler.querySelectorAll('[data-radio-trigger]'))
-            .map(el => {
-                return {
-                    wrapper: el,
-                    input: el.querySelector('input[type="radio"]')
-                }
-            });
-        
-        triggers.forEach(trigger => trigger.input?.checked && trigger.wrapper.nextElementSibling.style.setProperty('display', 'block'));
-
-        radioSpoiler.addEventListener('change', (e) => {
-            const isEventFromTrigger = triggers.find(t => t.input === e.target);
-            if(isEventFromTrigger) {
-                triggers.forEach(trigger => {
-                    if(trigger.input?.checked) {
-                        slideDown(trigger.wrapper.nextElementSibling, 300);
-                    } else {
-                        slideUp(trigger.wrapper.nextElementSibling, 300);
-                    }
-                })
-            }
-        })
-    })
-}
-	// ==== // components =====================================================
-
-
-
 	// ==== sections =====================================================
 	{
     const header = document.querySelector('[data-header]');
@@ -825,6 +749,15 @@ window.addEventListener("DOMContentLoaded", () => {
     const mobileMenu = document.querySelector('[data-mobile-menu]');
     if(mobileMenu) {
         const burger = document.querySelector('[data-action="toggle-show-mobile-menu"]');
+        const mobileMenuList = mobileMenu.querySelector('.mobile-menu__list');
+        const listItems = Array.from(mobileMenuList.children);
+
+        listItems.forEach(li => {
+            const link = li.firstElementChild;
+            if(link.nextElementSibling) {
+                link.setAttribute('data-spoiler-trigger', '');
+            }
+        })
 
         
         const setHeaderAsFixed = () => {
@@ -963,6 +896,81 @@ window.addEventListener("DOMContentLoaded", () => {
 //     }
 // }
 	// ==== // sections =====================================================
+
+	// ==== components =====================================================
+	{
+    const spoilers = document.querySelectorAll('[data-spoiler]');
+    if (spoilers.length) {
+        spoilers.forEach(spoiler => {
+            const swiper = spoiler.closest('[data-scroll-container]')?.swiper;
+            let isOneActiveItem = spoiler.dataset.spoiler.trim() === 'one' ? true : false;
+            let triggers = spoiler.querySelectorAll('[data-spoiler-trigger]');
+            if (triggers.length) {
+                triggers.forEach(trigger => {
+                    let parent = trigger.parentElement;
+                    let content = trigger.nextElementSibling;
+
+                    // init
+                    if (trigger.classList.contains('active')) {
+                        content.style.display = 'block';
+                        parent.classList.add('active');
+                    }
+
+                    trigger.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        parent.classList.toggle('active');
+                        trigger.classList.toggle('active');
+                        content && slideToggle(content, 300);
+                        swiper && setTimeout(() => swiper.update(), 300);
+
+                        if (isOneActiveItem) {
+                            triggers.forEach(i => {
+                                if (i === trigger) return;
+
+                                let parent = i.parentElement;
+                                let content = i.nextElementSibling;
+
+                                parent.classList.remove('active');
+                                i.classList.remove('active');
+                                content && slideUp(content, 300);
+                                swiper && setTimeout(() => swiper.update(), 300);
+                            })
+                        }
+                    })
+                })
+            }
+        })
+    }
+
+    const radioSpoilers = document.querySelectorAll('[data-radio-spoiler]');
+    radioSpoilers.forEach(radioSpoiler => {
+        let triggers = Array.from(radioSpoiler.querySelectorAll('[data-radio-trigger]'))
+            .map(el => {
+                return {
+                    wrapper: el,
+                    input: el.querySelector('input[type="radio"]')
+                }
+            });
+        
+        triggers.forEach(trigger => trigger.input?.checked && trigger.wrapper.nextElementSibling.style.setProperty('display', 'block'));
+
+        radioSpoiler.addEventListener('change', (e) => {
+            const isEventFromTrigger = triggers.find(t => t.input === e.target);
+            if(isEventFromTrigger) {
+                triggers.forEach(trigger => {
+                    if(trigger.input?.checked) {
+                        slideDown(trigger.wrapper.nextElementSibling, 300);
+                    } else {
+                        slideUp(trigger.wrapper.nextElementSibling, 300);
+                    }
+                })
+            }
+        })
+    })
+}
+	// ==== // components =====================================================
+
+	
 
 	document.body.classList.add('page-loaded');
 });
